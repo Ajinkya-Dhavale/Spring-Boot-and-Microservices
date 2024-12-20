dedea39a1b5421b47fdf018cc932442c67774535
Ajinkya-Dhavale <dhavaleajinkya9604@gmail.com>
2024-12-20 09:12:46 +0530

    commit

---
+++ 11_rest_api_user/src/main/java/com/ajinkya/entity/UserEntity.java
@@ -0,0 +1,55 @@
+package com.ajinkya.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_api")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Integer userId;
+	
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "user_age")
+	private String userAge;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(String userAge) {
+		this.userAge = userAge;
+	}
+
+	@Override
+	public String toString() {
+		return "UserEntity [userId=" + userId + ", userName=" + userName + ", userAge=" + userAge + "]";
+	}	
+	
+	
+}
